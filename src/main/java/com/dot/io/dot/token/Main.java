fe3150b462fe3bfba44b1321a0724081477f544b
Kacper Seredyn <kacper.seredyn@tink.com>
2023-05-23 13:53:16 +0200

    adjustments

--- src/main/java/com/dot/io/dot/token/Main.java
+++ src/main/java/com/dot/io/dot/token/Main.java
@@ -5,14 +5,16 @@ import io.token.proto.common.account.AccountProtos;
 import io.token.proto.common.alias.AliasProtos;
 import io.token.proto.common.security.SecurityProtos;
 import io.token.proto.common.transaction.TransactionProtos;
+import io.token.security.KeyStore;
+import io.token.security.TokenCryptoEngineFactory;
 import io.token.security.UnsecuredFileSystemKeyStore;
+import io.token.security.crypto.CryptoType;
 import io.token.tokenrequest.TokenRequest;
 import io.token.tokenrequest.TokenRequestResult;
 import io.token.tpp.Account;
 import io.token.tpp.Member;
 import io.token.tpp.Representable;
 import io.token.tpp.TokenClient;
-import lombok.extern.slf4j.Slf4j;
 import spark.Spark;
 
 import java.awt.*;
@@ -30,7 +32,6 @@ import static io.token.proto.common.alias.AliasProtos.Alias.Type.DOMAIN;
 import static io.token.proto.common.token.TokenProtos.TokenRequestPayload.AccessBody.ResourceType.*;
 import static io.token.util.Util.generateNonce;
 
-@Slf4j
 public class Main {
 
     private static final int PORT = 3000;
@@ -42,6 +43,7 @@ public class Main {
 
     public void start() throws Exception {
         TokenClient client = prepareClient();
+        // This member creation process would be replaced with loading registered secrets
         Member member = getMember(client);
 
         setupCallback(client, member);
@@ -52,21 +54,27 @@ public class Main {
     private void setupCallback(TokenClient client, Member member) {
         Spark.port(PORT);
 
-        Spark.get("/callback", (req, res) -> {
-            res.status(200);
-            return fetchData(client, member, req.queryParams("request-id"));
-        });
+        // Token redirects the user to this endpoint
+        Spark.get(
+                "/callback",
+                (req, res) -> {
+                    res.status(200);
+                    return fetchData(client, member, req.queryParams("request-id"));
+                });
     }
 
     private String fetchData(TokenClient client, Member member, String requestId) {
+        StringBuilder outputBuilder = new StringBuilder();
+        outputBuilder.append("<h1>Aggregation results</h1>");
+
+        // According to TokenClient.parseTokenRequestCallbackUrlBlocking, tokenId will be removed
+        // from the callback??
+        // The docs don't mention this, but to be on the safe side, requestId is used to get a
+        // tokenId.
         TokenRequestResult requestResult = client.getTokenRequestResultBlocking(requestId);
         String tokenId = requestResult.getTokenId();
 
         Representable representable = member.forAccessToken(tokenId);
-
-        StringBuilder outputBuilder = new StringBuilder();
-        outputBuilder.append("<h1>Aggregation results</h1>");
-
         fetchAccounts(representable, outputBuilder);
 
         return outputBuilder.toString();
@@ -77,8 +85,11 @@ public class Main {
 
         for (Account account : accounts) {
             AccountProtos.AccountDetails accountDetails = account.accountDetails();
-            TransactionProtos.Balance balance = account.getBalanceBlocking(SecurityProtos.Key.Level.PRIVILEGED);
 
+            // The docs don't say much about what the privilege levels are supposed to control.
+            // It won't matter if we registered one key with DCR.
+            TransactionProtos.Balance balance =
+                    account.getBalanceBlocking(SecurityProtos.Key.Level.PRIVILEGED);
 
             outputBuilder.append("<h2>Account</h2>");
 
@@ -98,7 +109,8 @@ public class Main {
             outputBuilder.append("<td>" + accountDetails.getAccountHolderName() + "</td>");
 
             outputBuilder.append("<td><ul>");
-            for (AccountProtos.AccountIdentifier identifier : accountDetails.getAccountIdentifiersList()) {
+            for (AccountProtos.AccountIdentifier identifier :
+                    accountDetails.getAccountIdentifiersList()) {
                 outputBuilder.append("<li>" + identifier.toString() + "</li>");
             }
             outputBuilder.append("</ul></td>");
@@ -113,17 +125,19 @@ public class Main {
 
             fetchTransactions(account, outputBuilder);
         }
-
     }
 
     private void fetchTransactions(Account account, StringBuilder outputBuilder) {
-        PagedList<TransactionProtos.Transaction, String> transactionPagedList = account.getTransactionsBlocking(null, 100, SecurityProtos.Key.Level.PRIVILEGED);
+        // Pagination would need to be implemented properly
+        PagedList<TransactionProtos.Transaction, String> transactionPagedList =
+                account.getTransactionsBlocking(null, 100, SecurityProtos.Key.Level.PRIVILEGED);
 
         outputBuilder.append("<h2>Transactions</h2>");
 
         outputBuilder.append("<table>");
         outputBuilder.append("<thead><tr>");
         outputBuilder.append("<th>ID</th>");
+        outputBuilder.append("<th>Type</th>");
         outputBuilder.append("<th>Status</th>");
         outputBuilder.append("<th>Amount</th>");
         outputBuilder.append("<th>Currency</th>");
@@ -138,12 +152,20 @@ public class Main {
             outputBuilder.append("<tr>");
 
             outputBuilder.append("<td>" + transaction.getId() + "</td>");
+            outputBuilder.append("<td>" + transaction.getType().name() + "</td>");
             outputBuilder.append("<td>" + transaction.getStatus().name() + "</td>");
             outputBuilder.append("<td>" + transaction.getAmount().getValue() + "</td>");
             outputBuilder.append("<td>" + transaction.getAmount().getCurrency() + "</td>");
             outputBuilder.append("<td>" + transaction.getCreatedAtMs() + "</td>");
-            outputBuilder.append("<td>" + transaction.getCreditorEndpoint().getAccountIdentifier() + "</td>");
-            outputBuilder.append("<td>" + transaction.getCreditorEndpoint().getCustomerData() + "</td>");
+
+            // This is kind of confusing, as it is not required (at least it does not appear when
+            // aggregating from NatWest Sandbox).
+            // Transaction.getProviderTransactionDetails seems to contain similar data, but it's
+            // bank-specific (and the CMA9 DTO is missing from the SDK for some reason).
+            outputBuilder.append(
+                    "<td>" + transaction.getCreditorEndpoint().getAccountIdentifier() + "</td>");
+            outputBuilder.append(
+                    "<td>" + transaction.getCreditorEndpoint().getCustomerData() + "</td>");
 
             outputBuilder.append("</tr>");
         }
@@ -151,11 +173,20 @@ public class Main {
     }
 
     private TokenClient prepareClient() throws IOException {
+        // The directory specified here will store the keys, getting rid of the need to create a new
+        // member every run.
         Path keysPath = Files.createDirectories(Paths.get("./keys"));
+        KeyStore keyStore = new UnsecuredFileSystemKeyStore(keysPath.toFile());
 
         return TokenClient.builder()
                 .connectTo(SANDBOX)
-                .withKeyStore(new UnsecuredFileSystemKeyStore(keysPath.toFile()))
+                .withCryptoEngine(
+                        new TokenCryptoEngineFactory(
+                                keyStore,
+                                CryptoType
+                                        .RS256)) // the default ED25519 is not supported in jwt.io,
+                // set to RS256 for convenience when constructing
+                // Postman calls using the keys generated here
                 .build();
     }
 
@@ -174,10 +205,8 @@ public class Main {
     private Member createMember(TokenClient client) {
         String email = generateNonce().toLowerCase() + "-test+noverify@example.com";
 
-        AliasProtos.Alias alias = AliasProtos.Alias.newBuilder()
-                .setType(DOMAIN)
-                .setValue(email)
-                .build();
+        AliasProtos.Alias alias =
+                AliasProtos.Alias.newBuilder().setType(DOMAIN).setValue(email).build();
 
         Member member = client.createMemberBlocking(alias);
         member.addRedirectUrlsBlocking(List.of(REDIRECT_URL));
@@ -188,12 +217,13 @@ public class Main {
     private String genereateRedirectUrl(TokenClient client, Member member) {
         String refId = generateNonce();
 
-        TokenRequest request = TokenRequest.accessTokenRequestBuilder(ACCOUNTS, BALANCES, TRANSACTIONS)
-                .setRefId(refId)
-                .setToMemberId(member.memberId())
-                .setRedirectUrl(REDIRECT_URL)
-                .setToAlias(member.firstAliasBlocking())
-                .build();
+        TokenRequest request =
+                TokenRequest.accessTokenRequestBuilder(ACCOUNTS, BALANCES, TRANSACTIONS)
+                        .setRefId(refId)
+                        .setToMemberId(member.memberId())
+                        .setRedirectUrl(REDIRECT_URL)
+                        .setToAlias(member.firstAliasBlocking())
+                        .build();
 
         String requestId = member.storeTokenRequestBlocking(request);
 
@@ -201,7 +231,8 @@ public class Main {
     }
 
     private void redirectToUrl(String url) throws Exception {
-        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+        if (Desktop.isDesktopSupported()
+                && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
             Desktop.getDesktop().browse(new URI(url));
         } else {
             System.out.println("REDIRECT!");
